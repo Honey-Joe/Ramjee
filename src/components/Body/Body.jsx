ed84828e7366d561a1202e3e7e7bef46fe87dded
SudharsanMaruthaiyan <sudharsan6078@gmail.com>
2024-09-20 19:18:38 +0530

    done

--- src/components/Body/Body.jsx
+++ src/components/Body/Body.jsx
@@ -1,9 +1,13 @@
 import ChooseUs from "../ChooseUs/ChooseUs"
 import ReviewSection from "../ReviewSection/ReviewSection"
+import Homepage from "./Homepage/Homepage"
+import UpcomingEvent from "./UpcomingEvent/UpcomingEvent"
 
 const Body = () => {
   return (
-    <>
+    <>  
+        <Homepage/>
+        <UpcomingEvent/>
         <ChooseUs></ChooseUs>
         <ReviewSection></ReviewSection>
     </>
